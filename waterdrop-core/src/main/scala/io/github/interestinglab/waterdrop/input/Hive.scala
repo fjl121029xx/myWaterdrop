db5b40050d9d937e4c431150389985410a1f5617
贾 全宇 <jiaquanyu@hualala.com>
2018-12-13 21:06:10 +0800

    update

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/Hive.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/input/Hive.scala
@@ -2,54 +2,54 @@ package io.github.interestinglab.waterdrop.input
 
 import com.typesafe.config.{Config, ConfigFactory}
 import io.github.interestinglab.waterdrop.apis.BaseStaticInput
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.{DataTypes, StructType}
+import org.apache.spark.sql.{Dataset, Row, RowFactory, SparkSession}
 
 class Hive extends BaseStaticInput {
 
   var config: Config = ConfigFactory.empty()
 
   /**
-   * Set Config.
-   */
+    * Set Config.
+    */
   override def setConfig(config: Config): Unit = {
     this.config = config
   }
 
   /**
-   * Get Config.
-   */
+    * Get Config.
+    */
   override def getConfig(): Config = {
     this.config
   }
 
   /**
-   * Get Dataset from this Static Input.
-   */
+    * Get Dataset from this Static Input.
+    */
   override def getDataset(spark: SparkSession): Dataset[Row] = {
 
-    val ds = spark.sql(buildSqlStr)
-    ds
-
+    spark.sql(buildSqlStr)
   }
 
   def buildSqlStr: String = {
 
-    val query = config.getString("query")
-    val database = config.getString("database")
-    val table = config.getString("table")
-    val columns = if (config.getString("columns").isEmpty) "*" else config.getString("columns")
-    val where = if (config.getString("where").isEmpty) "" else "WHERE " + config.getString("where")
-    val partition = if (config.getString("partition").isEmpty) "" else "AND " + config.getString("partition")
-
-    val sqlStr = if (query.isEmpty) s"SELECT $columns FROM $database.$table $where $partition" else query
-
-    sqlStr
-
+    config.hasPath("query") match {
+      case true => config.getString("query")
+      case false => {
+        val database = config.getString("database")
+        val table = config.getString("table")
+        val columns = if (config.getString("columns").isEmpty) "*" else config.getString("columns")
+        val where = if (config.getString("where").isEmpty) "" else "WHERE " + config.getString("where")
+        val partition = if (config.getString("partition").isEmpty) "" else "AND " + config.getString("partition")
+        s"SELECT $columns FROM $database.$table $where $partition"
+      }
+    }
   }
 
   /**
-   * Return true and empty string if config is valid, return false and error message if config is invalid.
-   */
+    * Return true and empty string if config is valid, return false and error message if config is invalid.
+    */
   override def checkConfig(): (Boolean, String) = {
 
     config.hasPath("query") match {
