72764e4179c839ccbe9c1cd190a215dafd472189
贾 全宇 <jiaquanyu@hualala.com>
2018-12-20 18:51:36 +0800

    数据去重

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Recent.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Recent.scala
@@ -1,10 +1,51 @@
 package io.github.interestinglab.waterdrop.filter
 
-/**
-  *
-  * @author jiaquanyu 
-  *
-  */
-object Recent {
+import com.typesafe.config.{Config, ConfigFactory}
+import io.github.interestinglab.waterdrop.apis.BaseFilter
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+
+class Recent extends BaseFilter {
+
+  var conf = ConfigFactory.empty
+
+  override def setConfig(config: Config): Unit = {
+    this.conf = config
+  }
+
+  override def getConfig(): Config = {
+    this.conf
+  }
+
+  override def checkConfig(): (Boolean, String) = {
+
+    conf.hasPath("union.fields") match {
+      case true => (true, "")
+      case false => (false, "union.fields is required !!!")
+    }
+  }
+
+  override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
+
+    val unionFieldArr = conf.getString("union.fields").split(",")
+
+    //add increasing unionKey
+    val increasingID = "datasets_increasing_id"
+    val unionKey = "datasets_union_key"
+
+    val dfi = df
+      .withColumn(increasingID, monotonically_increasing_id)
+      .withColumn(unionKey, concat(unionFieldArr.map(col(_)): _*))
+
+    //get recent data
+    val dfm = dfi
+      .groupBy(unionKey)
+      .max(increasingID)
+
+    //drop increasing and unionKey
+    dfm
+      .join(dfi, dfm(s"max($increasingID)") === dfi(increasingID) and dfm(unionKey) === dfi(unionKey))
+      .drop(s"max($increasingID)", increasingID, unionKey)
+  }
 
 }
