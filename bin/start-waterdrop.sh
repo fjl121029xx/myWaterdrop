b6e5efa5af1071e032ae4a262b2da29ea788bf1e
贾 全宇 <jiaquanyu@hualala.com>
2019-01-22 16:45:55 +0800

    add monitor

--- bin/start-waterdrop.sh
+++ bin/start-waterdrop.sh
@@ -23,6 +23,13 @@ while (( "$#" )); do
       shift 2
       ;;
 
+    -s|--metrics-sink)
+      METRICS_SINK=$2
+      CMD_ARGUMENTS=${CMD_ARGUMENTS//' '$1/''}
+      CMD_ARGUMENTS=${CMD_ARGUMENTS//' '$2/''}
+      shift 2
+      ;;
+
     --) # end argument parsing
       shift
       break
@@ -60,6 +67,10 @@ MASTER=${MASTER:-$DEFAULT_MASTER}
 DEFAULT_DEPLOY_MODE=client
 DEPLOY_MODE=${DEPLOY_MODE:-$DEFAULT_DEPLOY_MODE}
 
+# sink metrics 2 influxdb default false
+DEFAULT_METRICS_SINK=false
+METRICS_SINK=${METRICS_SINK:-$DEFAULT_METRICS_SINK}
+
 # scan jar dependencies for all plugins
 source ${UTILS_DIR}/file.sh
 source ${UTILS_DIR}/app.sh
@@ -70,6 +81,7 @@ if [ "$jarDependencies" != "" ]; then
 fi
 
 FilesDepOpts=""
+ConfDepOpts=""
 if [ "$DEPLOY_MODE" == "cluster" ]; then
 
     ## add config file
@@ -77,6 +89,18 @@ if [ "$DEPLOY_MODE" == "cluster" ]; then
 
     ## add plugin files
     FilesDepOpts="${FilesDepOpts},${APP_DIR}/plugins.tar.gz"
+
+    ## add kafka sasl conf
+    FilesDepOpts="${FilesDepOpts},${CONF_DIR}/kafka_client_jaas.conf"
+
+    if [ "$METRICS_SINK" == "true" ]; then
+       FilesDepOpts="${FilesDepOpts},${CONF_DIR}/metrics.properties"
+
+       ##add driver and executor extra class
+       DRIVER_EXTRACLASS="--conf spark.driver.extraClassPath=spark-influx-sink-0.4.0.jar:metrics-influxdb-1.1.8.jar"
+       EXECUTOR_DRIVER_EXTRACLASS="--conf spark.executor.extraClassPath=spark-influx-sink-0.4.0.jar:metrics-influxdb-1.1.8.jar"
+       ConfDepOpts="$DRIVER_EXTRACLASS $EXECUTOR_DRIVER_EXTRACLASS"
+    fi
 
     echo ""
 
@@ -93,6 +117,9 @@ exec ${SPARK_HOME}/bin/spark-submit --class io.github.interestinglab.waterdrop.W
     --name $(getAppName ${CONFIG_FILE}) \
     --master ${MASTER} \
     --deploy-mode ${DEPLOY_MODE} \
+    --driver-java-options "-Djava.security.auth.login.config=./kafka_client_jaas.conf" \
+    --conf "spark.executor.extraJavaOptions=-Djava.security.auth.login.config=./kafka_client_jaas.conf" \
     ${JarDepOpts} \
+    ${ConfDepOpts} \
     ${FilesDepOpts} \
     ${assemblyJarName} ${CMD_ARGUMENTS}
