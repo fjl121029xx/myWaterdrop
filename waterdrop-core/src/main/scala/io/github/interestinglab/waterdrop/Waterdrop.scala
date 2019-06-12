588266434b1dd562a8f675163bbf680615336f42
贾 全宇 <jiaquanyu@hualala.com>
2019-06-12 18:42:25 +0800

    remove influxdb writer

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/Waterdrop.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/Waterdrop.scala
@@ -6,17 +6,12 @@ import io.github.interestinglab.waterdrop.filter.UdfRegister
 import io.github.interestinglab.waterdrop.utils.AsciiArt
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.apache.hadoop.fs.Path
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.StringEntity
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.protocol.HTTP
+import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
-import org.apache.spark.scheduler.{SparkListener, SparkListenerTaskEnd}
 import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.types.{DataTypes, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.streaming._
-import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.JavaConversions._
 import scala.util.{Failure, Success, Try}
@@ -145,32 +140,7 @@ object Waterdrop extends Logging {
 
     streamingInputs.size match {
       case 0 => {
-
-        sparkSession.sparkContext.addSparkListener(new SparkListener() {
-          override def onTaskEnd(taskEnd: SparkListenerTaskEnd) {
-            val metrics = taskEnd.taskMetrics
-            if (metrics.inputMetrics != None) {
-              println("[DEBUG] metrics input: " + metrics.inputMetrics.recordsRead)
-              inputRecords += metrics.inputMetrics.recordsRead
-              inputBytes += metrics.inputMetrics.bytesRead
-            }
-            if (metrics.outputMetrics != None) {
-              println("[DEBUG] metrics output: " + metrics.outputMetrics.recordsWritten)
-              outputWritten += metrics.outputMetrics.recordsWritten
-              outputBytes += metrics.outputMetrics.bytesWritten
-            }
-          }
-        })
-
         batchProcessing(sparkSession, configBuilder, staticInputs, filters, outputs)
-
-        //write metrics to influxdb
-        write2Influx(sparkSession.sparkContext, inputRecords, inputBytes, outputWritten, outputBytes)
-
-        println("[INFO] input record size: " + inputRecords)
-        println("[INFO] input byte size: " + inputBytes)
-        println("[INFO] output write size: " + outputWritten)
-        println("[INFO] output byte size: " + outputBytes)
       }
       case _ => {
         streamingProcessing(sparkSession, configBuilder, staticInputs, streamingInputs, filters, outputs)
@@ -226,34 +196,30 @@ object Waterdrop extends Logging {
       }
 
       // For implicit conversions like converting RDDs to DataFrames
-
       val schema = new StructType().add("raw_message", DataTypes.StringType)
       val encoder = RowEncoder(schema)
       var ds = sparkSession.createDataset(rowsRDD)(encoder)
 
-      // Ignore empty schema dataset
-      if (ds.columns.length > 0 && !rowsRDD.isEmpty()) {
 
-        for (f <- filters) {
-          ds = f.process(sparkSession, ds)
-        }
-
-        streamingInputs.foreach(p => {
-          p.beforeOutput
-        })
-
-        try {
-          outputs.foreach(p => {
-            p.process(ds)
-          })
-        } catch {
-          case ex: Exception => throw ex
-        }
-
-        streamingInputs.foreach(p => {
-          p.afterOutput
-        })
+      for (f <- filters) {
+        ds = f.process(sparkSession, ds)
       }
+
+      streamingInputs.foreach(p => {
+        p.beforeOutput
+      })
+
+      try {
+        outputs.foreach(p => {
+          p.process(ds)
+        })
+      } catch {
+        case ex: Exception => throw ex
+      }
+
+      streamingInputs.foreach(p => {
+        p.afterOutput
+      })
     }
 
     ssc.start()
@@ -336,33 +302,6 @@ object Waterdrop extends Logging {
         System.exit(-1) // invalid configuration
       }
     }
-  }
-
-  private def write2Influx(
-    sc: SparkContext,
-    inputRecords: Long,
-    inputBytes: Long,
-    outputWritten: Long,
-    outputBytes: Long): Unit = {
-
-    val httpclient = HttpClients.createDefault
-
-    val url = "http://192.168.22.63:8086/write?db=waterdrop_batch"
-    val httpPost = new HttpPost(url){{
-      addHeader(HTTP.CONTENT_TYPE, "BINARY")
-    }}
-
-    val tags = s"appId=${sc.applicationId},appName=${sc.appName}," +
-      s"user=${sc.sparkUser},startTime=${sc.startTime},endTime=${System.currentTimeMillis}"
-    val fields = s"inputRecords=${inputRecords},inputBytes=${inputBytes},outputWritten=${outputWritten},outputBytes=${outputBytes}"
-    val text = s"waterdrop_report,${tags} ${fields}"
-    println("[INFO] influx data:" + text)
-
-    httpPost.setEntity(new StringEntity(text))
-    val response = httpclient.execute(httpPost)
-
-    println("[INFO] write 2 influxdb response:\n" + response)
-
   }
 
 }
