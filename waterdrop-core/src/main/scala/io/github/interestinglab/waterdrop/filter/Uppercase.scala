c6a0f8ae29422edea1d4917f340284e296d893db
jiaquanyu <jiaquanyu@hualala.com>
2019-11-06 10:33:52 +0800

    add schema uppercase/lowercase

--- waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Uppercase.scala
+++ waterdrop-core/src/main/scala/io/github/interestinglab/waterdrop/filter/Uppercase.scala
@@ -39,6 +39,9 @@ class Uppercase extends BaseFilter {
   }
 
   override def process(spark: SparkSession, df: Dataset[Row]): Dataset[Row] = {
-    df.withColumn(conf.getString("target_field"), upper(col(conf.getString("source_field"))))
+    conf.getString("source_field").equals("df.schema") match {
+      case true => df.select(df.columns.map(x =>col(x).as(x.toUpperCase)) :_*)
+      case false => df.withColumn(conf.getString("target_field"), upper(col(conf.getString("source_field"))))
+    }
   }
 }
